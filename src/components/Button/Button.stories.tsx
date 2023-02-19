c401484b783bcc7c0ed52ef8a46fbe187a65c6ba
ericstefano <ericstefano12@gmail.com>
2023-02-19 00:51:04 -0300

    feat: change storybook format

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from './Button';
+
+export default {
+  title: 'Button',
+  component: Button,
+  argTypes: { onClick: { action: 'clicked' } },
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args}>Button</Button>;
+
+export const Default = Template.bind({});
+Default.args = {};

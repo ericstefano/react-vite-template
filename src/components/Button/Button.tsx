ebbad19f970783b74410ebfd42af1c5220a7f35f
ericstefano <ericstefano12@gmail.com>
2023-03-17 12:42:23 -0300

    feat: move unocss config to unocss.config.ts

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -10,7 +10,6 @@ export const Button = ({ children, ...props }: ButtonProps) => {
       {...props}
       className='bg-blue-500 px-3 py-2 text-(white sm) rounded-md font-700 inline-flex gap-2 items-center'
     >
-      <div className='i-twemoji-grinning-face-with-smiling-eyes h-4 w-4' />
       {children}
     </button>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,10 @@
-import presetIcons from '@unocss/preset-icons';
-import presetUno from '@unocss/preset-uno';
-import presetWebFonts from '@unocss/preset-web-fonts';
-import transformerVariantGroup from '@unocss/transformer-variant-group';
 import UnoCSS from '@unocss/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    UnoCSS({
-      presets: [
-        presetUno(),
-        presetIcons({
-          extraProperties: {
-            display: 'inline-block',
-            'vertical-align': 'middle',
-          },
-        }),
-        presetWebFonts({
-          provider: 'google',
-          fonts: {
-            sans: ['Raleway:100,200,300,400,500,600,700'],
-          },
-        }),
-      ],
-      transformers: [transformerVariantGroup()],
-    }),
-  ],
+  plugins: [react(), UnoCSS()],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),

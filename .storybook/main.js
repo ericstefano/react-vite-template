b0756cd939821c28c8b19dc4e37b257cb7d41ad8
ericstefano <ericstefano12@gmail.com>
2023-04-07 12:48:28 -0300

    fix: remove unneeded storybook plugin

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,7 +6,6 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
-    '@storybook/addon-mdx-gfm',
   ],
   framework: {
     name: '@storybook/react-vite',

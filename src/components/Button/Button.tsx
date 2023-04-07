60769db5fb43407396d757760759ee5e1e6313d3
ericstefano <ericstefano12@gmail.com>
2023-04-07 12:02:24 -0300

    fix: eslint issues and simplify button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,30 +1,7 @@
 import { ComponentPropsWithoutRef } from 'react';
 
-type ButtonProps = {
-  color?: string;
-} & ComponentPropsWithoutRef<'button'>;
+type ButtonProps = ComponentPropsWithoutRef<'button'>;
 
-export const Button = ({
-  children,
-  color = '#1E8FD5',
-  ...props
-}: ButtonProps) => {
-  return (
-    <button
-      {...props}
-      style={{
-        backgroundColor: color,
-        color: '#FFFFFF',
-        border: 0,
-        padding: '12px 18px',
-        fontSize: '16px',
-        fontWeight: 'bold',
-        lineHeight: '16px',
-        borderRadius: '3px',
-        cursor: 'pointer',
-      }}
-    >
-      {children}
-    </button>
-  );
+export const Button = ({ children, ...props }: ButtonProps) => {
+  return <button {...props}>{children}</button>;
 };

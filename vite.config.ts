68f9b68c6d74652e3c804f2356c19c02f9e11e3d
ericstefano <ericstefano12@gmail.com>
2023-05-19 16:36:25 -0300

    feat: add vitest ui and move config

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
+/// <reference types="vitest" />
 import { resolve } from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+      '~mocks': resolve(__dirname, './mocks'),
     },
   },
 });
